6f478e4995175937d2be78cbf8368391967a976c
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-04-13 17:56:02 +0200

    println in both class'

--- 15 - Projektarbejde, GIT, GRASP/src/JensTest.java
+++ 15 - Projektarbejde, GIT, GRASP/src/JensTest.java
@@ -1,5 +1,7 @@
 public class JensTest {
   public static void main(String[] args) {
     System.out.println("Test2222gffgg22222");
+
+    System.out.println("wooohooo det virker!");
   }
 }
--- 15 - Projektarbejde, GIT, GRASP/src/teste1.java
+++ 15 - Projektarbejde, GIT, GRASP/src/teste1.java
@@ -1,5 +1,7 @@
 public class teste1 {
   public static void main(String[] args) {
     System.out.println("hfjdhjfd");
+
+    System.out.println("Yey det virker!");
   }
 }
